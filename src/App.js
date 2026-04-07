2b1dcc9398d2c8b2dc0cb931022111ec77a266c3
Dhiliban <dhiliptry7@gamil.com>
2026-04-07 15:42:53 +0530

    format

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ function App() {
   return (
     <div className="App">
       <>main branch</>
-
-
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
